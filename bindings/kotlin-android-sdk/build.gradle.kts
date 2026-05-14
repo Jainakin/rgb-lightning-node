7c96ef650451bf5355f751f2843f732d341a3a2d
s.migalaty <s.migalaty@gmail.com>
2026-05-14 17:56:58 +0300

    fix(android-sdk): align Java/Kotlin JVM target to 17

--- bindings/kotlin-android-sdk/build.gradle.kts
+++ bindings/kotlin-android-sdk/build.gradle.kts
@@ -18,6 +18,15 @@ android {
     namespace = "org.utexo.rgblightningnode"
     compileSdk = 34
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     defaultConfig {
         minSdk = 24
         consumerProguardFiles("consumer-rules.pro")
